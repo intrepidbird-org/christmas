657be4cb5629586bfe015ebe9285c5f64ce8c200
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-12-16 09:26:21 -0500

    Create missing-reindeer.py

---
+++ challenges/cyber/missing-reindeer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes
+
+e = 3
+d = -1
+
+while d == -1:
+    p = getPrime(1024)
+    q = getPrime(1024)
+    phi = (p - 1) * (q - 1)
+    d = inverse(e, phi)
+
+n = p * q
+
+message = b"🎅 [CENSORED] 🎅"
+holly = bytes_to_long(message)
+jolly = pow(pt, e, n)
+
+print(f"n = {n}")
+print(f"e = {e}")
+print(f"jolly = {jolly}")
