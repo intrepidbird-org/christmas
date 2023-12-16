90ccbe8fec635bb6a77f445f3c6e4986c85766a0
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-12-16 10:39:15 -0500

    Create christmas.js

---
+++ xmas-fun/christmas.js
@@ -0,0 +1,60 @@
+                     const
+                    lyrics =
+                   `All I Wa`+
+                 `nt For Chris`+
+                 `tmas Is You `+
+                    `by Mar`+
+                    `iah Car`+
+                   `ey:\nI d`+
+                 `on't want `+
+                 `a lot for Ch`+
+                `ristmas\nThere `+
+                `is just one thing`+
+               `I need\nI don't car`+
+           `e about the presents und`+
+                 `erneath the `+
+               `Christmas tree\n`+
+            `I just want you for my`+
+         `own\nMore than you could ev`+
+      `er know\nMake my wish come true\n`+
+     `All I want for Christmas is you\nYeah`+
+   `\nI don't want a lot for Christmas\nThere`+
+    `is just one thing I need (and I)\nDon't `+
+                    `care abou`+
+                    `t the pre`+
+                    `sents und`+
+                    `erneath t`+
+                    `he Christ`+
+                    `mas tree`+
+ `I don't need to hang my stocking there upon the fir`+
+ `eplace\nSanta Claus won't make me happy with a toy `+
+ `on Christmas Day\nI just want you for my own\nMore `+
+  `than you could ever know\nMake my wish come true\nA`+
+ `ll I want for Christmas is you\nYou, baby\nOh, I wo`+
+ `n't ask for much this Christmas\nI won't even wish `+
+ `for snow (and I)\nI'm just gonna keep on waiting un`+
+ `derneath the mistletoe\nI won't make a list and sen`+
+ `d it to the North Pole for Saint Nick\nI won't eve`+
+ `n stay awake to hear those magic reindeer click\n'`+
+ `Cause I just want you here tonight\nHolding on to `+
+ `me so tight\nWhat more can I do?\nOh, baby, all I `+
+ `want for Christmas is you\nYou, baby\nOh-oh, all t`+
+ `he lights are shining so brightly everywhere (so b`+
+ `rightly, baby)\nAnd the sound of children's laught`+
+ `er fills the air (oh, oh, yeah)\nAnd everyone is si`+
+ `nging (oh, yeah)\nI hear those sleigh bells ringing`+
+ `\nanta, won't you bring me the one I really need? (`+
+ `Yeah, oh)\nWon't you please bring my baby to me?\nO`+
+ `h, I don't want a lot for Christmas\nThis is all I'`+
+ `m asking for\nI just wanna see my baby standing rig`+
+ `ht outside my door\nOh, I just want you for my own\n`+
+ `More than you could ever know\nMake my wish come tru`+
+ `e\nOh, baby, all I want for Christmas is you\nYou, b`+
+ `aby\nAll I want for Christmas is you, baby\nAll I wa`+
+ `nt for Christmas is you, baby\nAll I want for Christ`+
+ `mas is you, baby\nAll I want for Christmas (all I re`+
+ `ally want) is you, baby\nAll I want (I want) for Chr`+
+ `istmas (all I really want) is you, baby `;console.log
+ (lyrics);/*Merry Christmas 2023 from BRCS! HoHoHo🎅🎅*/
+
+// From https://github.com/brcomputerscience-org/christmas-tree/blob/main/main.js
